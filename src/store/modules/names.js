a3a4a70f4dcd517f5be0ef7021b5a6440d9edd62
Henri Ihalainen <henri.ihalainen@gofore.com>
2018-04-22 08:40:00 +0300

    Improve rendering performance

--- src/store/modules/names.js
+++ src/store/modules/names.js
@@ -1,24 +1,31 @@
 import namedataApi from '../../api/namedata'
+import _ from 'lodash'
 
 const state = {
   forenames: [],
+  filteredForenames: [],
   filters: {
     minCount: 1000
   }
 }
 
 const getters = {
-  forenames: (state) => state.forenames.filter(name => name.total >= state.filters.minCount),
+  forenames: (state) => state.filteredForenames,
   minCount: (state) => state.filters.minCount
 }
+
+const debouncedFilter = _.debounce((commit) => commit('filter'), 500)
 
 const actions = {
   async loadForenames ({commit}) {
     commit('setForenames', await namedataApi.getForenames())
+    commit('filter')
   },
 
   setMinCount ({commit}, minCount) {
     commit('setMinCount', minCount)
+
+    debouncedFilter(commit)
   }
 }
 
@@ -29,6 +36,10 @@ const mutations = {
 
   setMinCount (state, minCount) {
     state.filters.minCount = minCount
+  },
+
+  filter (state) {
+    state.filteredForenames = state.forenames.filter(name => name.total >= state.filters.minCount)
   }
 }
 
